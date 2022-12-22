e4fd75b7bd9d006f7840062ec783fdf27db0f3c2
Grant <h.grant.horner@gmail.com>
2022-12-22 16:24:19 -0500

    finish day 7 part 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![warn(dead_code, unused_variables)]
+#![allow(dead_code, unused_variables)]
 
 pub mod one;
 pub mod two;
@@ -6,6 +6,7 @@ pub mod three;
 pub mod four;
 pub mod five;
 pub mod six;
+pub mod seven;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
